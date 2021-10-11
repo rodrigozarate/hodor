1faa2e33ad66bd44aeba50363a8217cc3784dde1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-11 03:05:10 -0500

    [UPDATE]

--- level_1/voter2.py
+++ level_1/voter2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-sitetoload = 'http://158.69.76.135/level0.php'
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+sitetoload = 'http://158.69.76.135/level1.php'
 
 def gecko_test(site=sitetoload):
     """
@@ -13,10 +17,12 @@ def gecko_test(site=sitetoload):
 
     # load the site
     driver.get(site)
+    wait = WebDriverWait(driver, 4)
     # set the value
     for x in range(0, 4096):
         # find the input text
         theid = driver.find_element_by_name('id')
+        wait.until(EC.presence_of_element_located((By.NAME, 'id')));
         theid.send_keys("3360")
         # send the form
         theid.send_keys(Keys.RETURN)
